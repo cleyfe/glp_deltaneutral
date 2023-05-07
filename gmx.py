456f3e32cd8027aa45ebb39c20e6b831f77f098d
Clement Felley <clement.fel@gmail.com>
2023-05-07 21:27:36 +0200

    Get GLP allocations

--- gmx.py
+++ gmx.py
@@ -29,12 +29,17 @@ arbiw3.eth.default_account = account.address
 print("Your hot wallet address is " , arbiw3.eth.default_account)
 
 
-# Addresses
+# Addresses (Used only as a reference. The contracts used are dynamically fetched using the wl_token_address function)
 AddressZero = "0x0000000000000000000000000000000000000000"
 weth = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1"
 btc = "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f"
 usdc = "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8"
 usdt = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"
+link = "0xf97f4df75117a78c1a5a0dbb814af92458539fb4"
+uni = "0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0"
+mim = "0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A"
+frax = "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F"
+dai = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1"
 gmx_vault = "0x489ee077994B6658eAfA855C308275EAd8097C4A"
 gmx_router = "0xaBBc5F99639c9B6bCb58544ddf04EFA6802F4064"
 gmx_reader = "0x22199a49A999c351eF7927602CFB187ec3cae489"
@@ -42,26 +47,43 @@ gmx_orderbook = "0x09f77E8A13De9a35a7231028187e9fD5DB8a2ACB"
 gmx_position_router = "0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba"
 chainlink_btc_addr = "0x6ce185860a4963106506C203335A2910413708e9"
 chainlink_eth_addr = "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612"
+chainlink_link_addr = "0x86E53CF1B870786351Da77A57575e79CB55812CB"
+chainlink_uni_addr = "0x9C917083fDb403ab5ADbEC26Ee294f6EcAda2720"
 
 # Contracts
 weth_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(weth.lower()), abi=abis.ERC20)
 btc_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(btc.lower()), abi=abis.ERC20)
 usdc_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(usdc.lower()), abi=abis.ERC20)
 usdt_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(usdt.lower()), abi=abis.ERC20)
+link_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(link.lower()), abi=abis.ERC20)
+uni_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(uni.lower()), abi=abis.ERC20)
+mim_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(mim.lower()), abi=abis.ERC20)
+frax_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(frax.lower()), abi=abis.ERC20)
+dai_contract = arbiw3.eth.contract(address=Web3.to_checksum_address(dai.lower()), abi=abis.ERC20)
 vault_contract = arbiw3.eth.contract(address=gmx_vault, abi=abis.GMX_VAULT)
 reader_contract = arbiw3.eth.contract(address=gmx_reader, abi=abis.GMX_READER)
 orderbook_contract = arbiw3.eth.contract(address=gmx_orderbook, abi=abis.GMX_ORDERBOOK)
 position_router_contract = arbiw3.eth.contract(address=gmx_position_router, abi=abis.GMX_POSITION_ROUTER)
 chainlink_btc_contract = arbiw3.eth.contract(address=chainlink_btc_addr, abi=abis.CHAINLINK)
 chainlink_eth_contract = arbiw3.eth.contract(address=chainlink_eth_addr, abi=abis.CHAINLINK)
+chainlink_link_contract = arbiw3.eth.contract(address=chainlink_link_addr, abi=abis.CHAINLINK)
+chainlink_uni_contract = arbiw3.eth.contract(address=chainlink_uni_addr, abi=abis.CHAINLINK)
 
 # Chainlink datafeed to get current price of tokens
 def getPrice(coin):
-    if (coin == "btc"):
-        latestData = chainlink_btc_contract.functions.latestRoundData().call()
+    match coin:
 
-    elif (coin == "eth"):
-        latestData = chainlink_eth_contract.functions.latestRoundData().call()
+        case "btc":
+            latestData = chainlink_btc_contract.functions.latestRoundData().call()
+
+        case "eth":
+            latestData = chainlink_eth_contract.functions.latestRoundData().call()
+        
+        case "link":
+            latestData = chainlink_link_contract.functions.latestRoundData().call()
+
+        case "uni":
+            latestData = chainlink_uni_contract.functions.latestRoundData().call()
 
     return latestData[1]/100000000.0
 
@@ -118,8 +140,36 @@ def retrieve_allocation():
 
     return [usdc_balance, weth_balance, btc_balance, usdt_balance]
 
+
+## IVault contract functions
 # Retrieve balance directly from the vault contract
 # It returns the same as retrieve_allocation, except we're looking at the vault contract for the token address, instead of looking at the token contract for the vault's address
 def token_balances():
     balance = vault_contract.functions.tokenBalances(Web3.to_checksum_address(weth.lower())).call() / (10 ** 18)
     return balance
+
+# Returns the number of tokens whitelisted in the vault
+def wl_tokens():
+    nr_tokens = vault_contract.functions.allWhitelistedTokensLength().call()
+    return nr_tokens
+
+# Returns the address of the designated token and whether it is a stablecoin
+def wl_token_address(i):
+    token_address = vault_contract.functions.allWhitelistedTokens(i).call()
+    is_stable = vault_contract.functions.stableTokens(token_address).call()
+    return token_address, is_stable
+
+# Returns the USD amount of a token in the vault
+def pool_amounts(address):
+    pool_amounts = vault_contract.functions.poolAmounts(address).call()
+    decimals = vault_contract.functions.tokenDecimals(address).call()
+    return pool_amounts / 10 ** decimals
+
+def token_weights(address):
+    token_weights = vault_contract.functions.tokenWeights(address).call()
+    return token_weights / 100
+    
+def get_price(address):
+    prices = reader_contract.functions.getPrices( Web3.to_checksum_address(gmx_reader.lower()), [ Web3.to_checksum_address(address.lower()) ] ).call()
+    return prices
+    
--- main.py
+++ main.py
@@ -1,10 +1,99 @@
 import gmx
 
+# Warning: this function is not dynamic if GMX adds new tokens to its whitelist!! (It will return an error)
+def match_chainlink_price(address):
+    match address.lower():
+        case '0x82af49447d8a07e3bd95bd0d56f35241523fbab1': #weth
+            price = eth_price
+            symbol = 'weth'
+
+        case '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f': #btc
+            price = btc_price
+            symbol = 'btc'
+
+
+        case '0xf97f4df75117a78c1a5a0dbb814af92458539fb4': #link
+            price = link_price
+            symbol = 'link'
+
+        case '0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0': #uni
+            price = uni_price
+            symbol = 'uni'
+
+        case _:
+            print("ERROR: A token was not matched by a chainlink data feed")
+    
+    return price, symbol
+
+
 #######################################################
 # Get current tokens prices
 #######################################################
-# btc_price = gmx.getPrice("btc")
-# eth_price = gmx.getPrice("eth")
+btc_price = gmx.getPrice("btc")
+eth_price = gmx.getPrice("eth")
+link_price = gmx.getPrice("link")
+uni_price = gmx.getPrice("uni")
+
+
+######################################################
+#Get GLP vault allocations
+#######################################################
+## Easy method but not dynamic:
+#glp_alloc = gmx.retrieve_allocation()
+#print(glp_alloc)
+
+## Dynamic method: 
+# 1) Determine the number of tokens that are whitelisted by GMX protocol
+# 2) Get the address of each whitelisted token and whether it is a stablecoin
+# 3) Get the quantity of the token in the GLP vault
+# 4) Get the token price from Chainlink feed
+# 5) Get the current weight of each token in the vault by multiplying Q*P and dividing by the total USD amount in the vault
+
+# 1) Determine the number of tokens that are whitelisted by GMX protocol
+nr_wl_tokens = gmx.wl_tokens()
+
+glp_data = {}
+
+for i in range(0, nr_wl_tokens):
+    # 2) Get the address of each whitelisted token and whether it is a stablecoin
+    address, stable = gmx.wl_token_address(i)
+    print("Token:", address)
+    print("Stablecoin:", stable)
+    # 3) Get the quantity of the token in the GLP vault
+    quantity = gmx.pool_amounts(address)
+    print("Quantity in the vault:", quantity)
+    #print("Target weight:", gmx.token_weights(address), "\n")
+
+    # 4) Get the token price from Chainlink feed
+    if not stable:
+        token_price, symbol = match_chainlink_price(address)
+        print("Token price:", token_price)
+        print("USD amount in the vault:", quantity * token_price, "\n")
+        glp_data[symbol] = {
+            'Address': address,
+            'Price': token_price,
+            'Quantity': quantity,
+            'TVL': quantity * token_price
+        }
+
+    else:
+        print("USD amount in the vault:", quantity, "\n") #stable assumed to be $1
+        glp_data[symbol] = {
+            'Address': address,
+            'Price': 1,
+            'Quantity': quantity,
+            'TVL': quantity * token_price
+        }
+        
+
+print(glp_data)
+glp_tvl = sum(coin['TVL'] for coin in glp_data.values())
+
+print("Total GLP TVL:", glp_tvl)
+
+
+
+
 
 #######################################################
 # Get user's wallet balances
@@ -28,12 +117,3 @@ import gmx
 #current_pos = gmx.getPosition( gmx.account.address, "eth", "weth", True)
 #print(current_pos)
 
-######################################################
-#Get GLP vault allocations
-#######################################################
-glp_alloc = gmx.retrieve_allocation()
-print(glp_alloc)
-
-
-weth_balance = gmx.token_balances()
-print("wETH balance in the vault:", weth_balance)
